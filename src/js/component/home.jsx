b940bfcf48457c4f5a950c53c2f67525a7603408
Daniel Perdomo <136826084+GoblinSwarm@users.noreply.github.com>
2024-07-09 20:01:06 +0000

    Fixed Stoplight

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,35 +3,22 @@ import React, { useState } from "react";
 const Home = () => {
     const [selectedColor, setSelectedColor] = useState("red");
 
-    const toggleColor = () => {
-        if (selectedColor === "red") {
-            setSelectedColor("yellow");
-        } else if (selectedColor === "yellow") {
-            setSelectedColor("green");
-        } else if (selectedColor === "green") {
-            setSelectedColor("purple");
-        } else {
-            setSelectedColor("red");
-        }
-    };
-
     return (
-        <div className="container d-flex justify-content-center">
-            <div className="row">
-                <div className="col-md-12">
-                    <div className="stoplight">
-                        <div 
-                            onClick={() => setSelectedColor("red")}
-                            className={"light red" + (selectedColor === "red" ? "glow" : "")}>
-                        </div>
-                        <div 
-                            onClick={() => setSelectedColor("yellow")}
-                            className={"light yellow" + (selectedColor === "yellow" ? "glow" : "")}>
-                        </div>
-                        <div
-                            onClick={() => setSelectedColor("green")}
-                            className={"light green" + (selectedColor === "green" ? "glow" : "")}>
-                        </div>
+        <div className="container d-flex justify-content-center align-items-center">
+            <div className="stoplight-container">
+                <div className="post"></div> {/* Este es el poste */}
+                <div className="stoplight">
+                    <div 
+                        onClick={() => setSelectedColor("red")}
+                        className={"light red" + (selectedColor === "red" ? " glow" : "")}>
+                    </div>
+                    <div 
+                        onClick={() => setSelectedColor("yellow")}
+                        className={"light yellow" + (selectedColor === "yellow" ? " glow" : "")}>
+                    </div>
+                    <div
+                        onClick={() => setSelectedColor("green")}
+                        className={"light green" + (selectedColor === "green" ? " glow" : "")}>
                     </div>
                 </div>
             </div>
